b8158783ad28e77aa053675737f0e3a455231b5b
Nicholas Petrone <nickpetrone92@gmail.com>
2020-02-06 22:53:15 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,14 @@
 class Dog
   def initialize(name, breed)
     @name = name
-    @breed = breed
+    @breed = breed = "Mutt"
   end
   
   def name=(name)
     @name = name
   end
   
-  def breed=(breed = "Mutt")
+  def breed=(breed)
     @breed = breed
   end
   
